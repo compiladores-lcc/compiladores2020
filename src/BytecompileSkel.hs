384dc3bc8da4ced66d9b9dcb19a4fa157883ef46
Guido Martínez <mtzguido@gmail.com>
2020-10-16 13:19:29 -0300

    Esqueleto Bytecompile

---
+++ src/BytecompileSkel.hs
@@ -0,0 +1,91 @@
+{-# LANGUAGE PatternSynonyms #-}
+{-|
+Module      : Byecompile
+Description : Compila a bytecode. Ejecuta bytecode.
+Copyright   : (c) Mauro Jaskelioff, Guido Martínez, 2020.
+License     : GPL-3
+Maintainer  : mauro@fceia.unr.edu.ar
+Stability   : experimental
+
+Este módulo permite compilar módulos a la BVM. También provee una implementación de la BVM 
+para ejecutar bytecode.
+-}
+module Bytecompile
+  (Bytecode, bytecompileModule, runBC, bcWrite, bcRead)
+ where
+
+import Lang 
+import Subst
+import MonadPCF
+
+import qualified Data.ByteString.Lazy as BS
+import Data.Binary ( Word32, Binary(put, get), decode, encode )
+import Data.Binary.Put ( putWord32le )
+import Data.Binary.Get ( getWord32le, isEmpty )
+
+type Opcode = Int
+type Bytecode = [Int]
+
+newtype Bytecode32 = BC { un32 :: [Word32] }
+
+{- Esta instancia explica como codificar y decodificar Bytecode de 32 bits -}
+instance Binary Bytecode32 where
+  put (BC bs) = mapM_ putWord32le bs
+  get = go 
+    where go =  
+           do
+            empty <- isEmpty
+            if empty
+              then return $ BC []
+              else do x <- getWord32le
+                      BC xs <- go
+                      return $ BC (x:xs)
+
+{- Estos sinónimos de patrón nos permiten escribir y hacer
+pattern-matching sobre el nombre de la operación en lugar del código
+entero, por ejemplo:
+ 
+   f (CALL : cs) = ...
+
+ Notar que si hubieramos escrito algo como
+   call = 5
+ no podríamos hacer pattern-matching con `call`.
+
+ En lo posible, usar estos códigos exactos para poder ejectutar un
+ mismo bytecode compilado en distintas implementaciones de la máquina.
+-}
+pattern RETURN   = 1
+pattern CONST    = 2
+pattern ACCESS   = 3
+pattern FUNCTION = 4
+pattern CALL     = 5
+pattern SUCC     = 6
+pattern PRED     = 7
+pattern IFZ      = 8
+pattern FIX      = 9
+pattern STOP     = 10
+pattern JUMP     = 11
+pattern SHIFT    = 12
+pattern DROP     = 13
+pattern PRINT    = 14
+
+bc :: MonadPCF m => Term -> m Bytecode
+bc t = error "implementame"
+
+bytecompileModule :: MonadPCF m => Module -> m Bytecode
+bytecompileModule mod = error "implementame"
+
+-- | Toma un bytecode, lo codifica y lo escribe un archivo 
+bcWrite :: Bytecode -> FilePath -> IO ()
+bcWrite bs filename = BS.writeFile filename (encode $ BC $ fromIntegral <$> bs)
+
+---------------------------
+-- * Ejecución de bytecode
+---------------------------
+
+-- | Lee de un archivo y lo decodifica a bytecode
+bcRead :: FilePath -> IO Bytecode
+bcRead filename = map fromIntegral <$> un32  <$> decode <$> BS.readFile filename
+
+runBC :: MonadPCF m => Bytecode -> m ()
+runBC c = error "implementame"
